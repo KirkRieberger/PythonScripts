6ee69d9d39ef8b963919a915d66b7d5fb30a2e1c
Kirk Rieberger <133918579+KirkRieberger@users.noreply.github.com>
2023-12-06 13:48:58 -0700

    Put data aquisition in a func

--- RadioWebScraper/FMScrape.py
+++ RadioWebScraper/FMScrape.py
@@ -4,7 +4,6 @@
 # See LICENCE.txt for full license
 
 #TODO: Command line args to select province, AM/FM/Both
-
 import requests
 import re
 import time
@@ -28,74 +27,80 @@ class DataSources:
 
 sources = DataSources()
 
-start = time.perf_counter()
 
-url = sources.alberta
+def getData(url):
+    start = time.perf_counter()
+    page = requests.get(url)
+    print('Requesting data from Alberta radio directory...')
+    if page.status_code == requests.codes.ok:
+        print('Connection successful!')
+    else:
+        print('Error connecting to site')
+        sys.exit(1)
 
-page = requests.get(url)
-print('Requesting data from Alberta radio directory...')
-if page.status_code == requests.codes.ok:
-    print('Connection successful!')
-else:
-    print('Error connecting to site')
-    sys.exit(1)
+    soup = bs(page.text, 'lxml')
 
-soup = bs(page.text, 'lxml')
+    table = soup.find('table')
+    rows = table.find_all('tr')
 
-table = soup.find('table')
-rows = table.find_all('tr')
+    # Row 0 is spacing
+    # Row 1 is province/territory heading and date
+    # Row 2 is spacing
+    # Row 3 is column labels
+    # Row 4 and beyond are radio station data
 
-# Row 0 is spacing
-# Row 1 is site heading and date
-# Row 2 is spacing
-# Row 3 is column labels
-# Row 4 and beyond are radio station data
+    file = open('AltaRadioStations.txt', 'w', encoding='UTF-8')
 
-file = open('AltaRadioStations.txt', 'w', encoding='UTF-8')
+    date = str(rows[1].find_all('td')[3])
+    # Regex to strip HTML tags from the date
+    date = re.sub('<[^<]+?>', '', date)
 
-date = str(rows[1].find_all('td')[3])
-# Regex to strip HTML tags from the date
-date = re.sub('<[^<]+?>', '', date)
+    file.write('Date Updated: ' + date + '\n\n')
 
-file.write('Date Updated: ' + date + '\n\n')
-
-i = 4
-while (i < len(list(rows))):
-    # Parse table data (td) fields, skipping programming format, station name,
-    # and station call sign, one row at a time
-    temp = rows[i].find_all('td')
-    out = []
-    j = 0
-    while (j < 8):
-        # Skip programming format, station name, and call sign
-        if j == 3 or j == 4 or j == 5:
+    i = 4
+    while (i < len(list(rows))):
+        # Parse table data (td) fields, skipping programming format, station name,
+        # and station call sign, one row at a time
+        temp = rows[i].find_all('td')
+        out = []
+        j = 0
+        while (j < 8):
+            # Skip programming format, station name, and call sign
+            if j == 3 or j == 4 or j == 5:
+                j += 1
+                continue
+            out.append(temp[j].text)
             j += 1
+        # Exit at first spacing line after data lines
+        if out == ['', '', '', '', '']:
+            break
+        # Don't care about AM stations
+        elif out[2] == 'AM' or out[2] == 'HD1' or out[2] == 'HD2':
+            i += 1
             continue
-        out.append(temp[j].text)
-        j += 1
-    # Exit at first spacing line after data lines
-    if out == ['', '', '', '', '']:
-        break
-    # Don't care about AM stations
-    elif out[2] == 'AM' or out[2] == 'HD1' or out[2] == 'HD2':
-        i += 1
-        continue
-    # Convert to standard power units
-    elif out[4] == 'kW':
-        out[3] = int(float(out[3])*1000)
-        out[4] = 'w'
-    k = 0
-    while k < 5:
-        if k == 2:
+        # Convert to standard power units
+        elif out[4] == 'kW':
+            out[3] = int(float(out[3])*1000)
+            out[4] = 'w'
+        k = 0
+        while k < 5:
+            if k == 2:
+                k += 1
+                continue
+            file.write('%s%s' % (out[k], ' '))  # f string
             k += 1
-            continue
-        file.write('%s%s' % (out[k], ' '))  # f string
-        k += 1
-    file.write('\n')
-    i += 1
+        file.write('\n')
+        i += 1
 
-file.close()
+    file.close()
 
-end = time.perf_counter()
-elapsed = round(end - start, 2)
-print(f'\nElapsed time: {elapsed}s')
+    end = time.perf_counter()
+    elapsed = round(end - start, 2)
+    print(f'\nElapsed time: {elapsed}s')
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
