914eea9640bd32055e4f7eaa93bf6dd173beef96
Kirk Rieberger <133918579+KirkRieberger@users.noreply.github.com>
2023-12-06 14:14:31 -0700

    Add argument parser

--- RadioWebScraper/FMScrape.py
+++ RadioWebScraper/FMScrape.py
@@ -3,63 +3,65 @@
 # Issued under GPLv2 or later
 # See LICENCE.txt for full license
 
-#TODO: Command line args to select province, AM/FM/Both
-#TODO: Command line args to select columns (Location, Frequency, Band, Station Name, Format, Call Sign, Power)
+# TODO: Command line args to select province, AM/FM/Both
+# TODO: Command line args to select columns (Location, Frequency, Band, Station Name, Format, Call Sign, Power)
 import requests
+import argparse
 import time
 import sys
 from bs4 import BeautifulSoup as bs
 
-class DataSources:
-    bc_url = 'https://www.canadianradiodirectory.com/british-columbia/'
-    bc = 'BC'
-    ab_url = 'https://www.canadianradiodirectory.com/alberta/'
-    ab = 'Alberta'
-    sk_url = 'https://www.canadianradiodirectory.com/saskatchewan/'
-    sk = 'Saskatchewan'
-    mb_url = 'https://www.canadianradiodirectory.com/manitoba/'
-    mb = 'Manitoba'
-    on_url = 'https://www.canadianradiodirectory.com/ontario/'
-    on = 'Ontario'
-    qc_url = 'https://www.canadianradiodirectory.com/quebec/'
-    qc = 'Quebec'
-    nb_url = 'https://www.canadianradiodirectory.com/new-brunswick/'
-    nb = 'NewBrunswick'
-    ns_url = 'https://www.canadianradiodirectory.com/nova-scotia/'
-    ns = 'NovaScotia'
-    pe_url = 'https://www.canadianradiodirectory.com/prince-edward-island/'
-    pe = 'PEI'
-    nl_url = 'https://www.canadianradiodirectory.com/newfoundland-labrador/'
-    nl = 'Newfoundland-Labrador'
-    yt_url = 'https://www.canadianradiodirectory.com/yukon/'
-    yt = 'Yukon'
-    nt_url = 'https://www.canadianradiodirectory.com/northwest-territories/'
-    nt = 'NorthwestTerritories'
-    nu_url = 'https://www.canadianradiodirectory.com/nunavut/'
-    nu = 'Nunavut'
 
-    
+class DataSources:
+    bc_url = "https://www.canadianradiodirectory.com/british-columbia/"
+    bc = "BC"
+    ab_url = "https://www.canadianradiodirectory.com/alberta/"
+    ab = "Alberta"
+    sk_url = "https://www.canadianradiodirectory.com/saskatchewan/"
+    sk = "Saskatchewan"
+    mb_url = "https://www.canadianradiodirectory.com/manitoba/"
+    mb = "Manitoba"
+    on_url = "https://www.canadianradiodirectory.com/ontario/"
+    on = "Ontario"
+    qc_url = "https://www.canadianradiodirectory.com/quebec/"
+    qc = "Quebec"
+    nb_url = "https://www.canadianradiodirectory.com/new-brunswick/"
+    nb = "NewBrunswick"
+    ns_url = "https://www.canadianradiodirectory.com/nova-scotia/"
+    ns = "NovaScotia"
+    pe_url = "https://www.canadianradiodirectory.com/prince-edward-island/"
+    pe = "PEI"
+    nl_url = "https://www.canadianradiodirectory.com/newfoundland-labrador/"
+    nl = "Newfoundland-Labrador"
+    yt_url = "https://www.canadianradiodirectory.com/yukon/"
+    yt = "Yukon"
+    nt_url = "https://www.canadianradiodirectory.com/northwest-territories/"
+    nt = "NorthwestTerritories"
+    nu_url = "https://www.canadianradiodirectory.com/nunavut/"
+    nu = "Nunavut"
+
+
 sources = DataSources()
 
 
 def getData(url, prov):
     page = requests.get(url)
-    print(f'Requesting data from {prov} radio directory...')
+    print(f"Requesting data from {prov} radio directory...")
     if page.status_code == requests.codes.ok:
-        print('Connection successful!')
+        print("Connection successful!")
     else:
-        print('Error connecting to site')
+        print("Error connecting to site")
         sys.exit(1)
 
-    soup = bs(page.text, 'lxml')
+    soup = bs(page.text, "lxml")
 
-    table = soup.find('table')
-    rows = table.find_all('tr')
+    table = soup.find("table")
+    rows = table.find_all("tr")
 
     return rows
 
-def parseData(rows):
 
+def parseData(rows):
     # Row 0 is spacing
     # Row 1 is province/territory heading and date
     # Row 2 is spacing
@@ -70,17 +72,17 @@ def parseData(rows):
 
     outputBuffer = ""
 
-    date = str(rows[1].find_all('td')[3].text)
-    outputBuffer += f'Date Updated: {date}\n\n'
+    date = str(rows[1].find_all("td")[3].text)
+    outputBuffer += f"Date Updated: {date}\n\n"
 
     i = 4
-    while (i < len(list(rows))):
+    while i < len(list(rows)):
         # Parse table data (td) fields, skipping programming format, station name,
         # and station call sign, one row at a time
-        temp = rows[i].find_all('td')
+        temp = rows[i].find_all("td")
         out = []
         j = 0
-        while (j < 8):
+        while j < 8:
             # Skip programming format, station name, and call sign
             if j == 3 or j == 4 or j == 5:
                 j += 1
@@ -88,24 +90,24 @@ def parseData(rows):
             out.append(temp[j].text)
             j += 1
         # Exit at first spacing line after data lines
-        if out == ['', '', '', '', '']:
+        if out == ["", "", "", "", ""]:
             break
         # Don't care about AM stations
-        elif out[2] == 'AM' or out[2] == 'HD1' or out[2] == 'HD2':
+        elif out[2] == "AM" or out[2] == "HD1" or out[2] == "HD2":
             i += 1
             continue
         # Convert to standard power units
-        elif out[4] == 'kW':
-            out[3] = int(float(out[3])*1000)
-            out[4] = 'w'
+        elif out[4] == "kW":
+            out[3] = int(float(out[3]) * 1000)
+            out[4] = "w"
         k = 0
         while k < 5:
             if k == 2:
                 k += 1
                 continue
-            outputBuffer += f'{out[k] }' #('%s%s' % (out[k], ' '))  # f string
+            outputBuffer += f"{out[k] }"  # ('%s%s' % (out[k], ' '))  # f string
             k += 1
-        outputBuffer += '\n'
+        outputBuffer += "\n"
         i += 1
 
     return outputBuffer
@@ -113,9 +115,26 @@ def parseData(rows):
 
 
 def main():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-bc", "--BritishColumbia", action="store_true", help="")
+    parser.add_argument("-ab", "--Alberta", action="store_true", help="")
+    parser.add_argument("-sk", "--Saskatchewan", action="store_true", help="")
+    parser.add_argument("-mb", "--Manitoba", action="store_true", help="")
+    parser.add_argument("-on", "--Ontario", action="store_true", help="")
+    parser.add_argument("-qc", "--Quebec", action="store_true", help="")
+    parser.add_argument("-nb", "--NewBrunswick", action="store_true", help="")
+    parser.add_argument("-ns", "--NovaScotia", action="store_true", help="")
+    parser.add_argument("-pe", "--PEI", action="store_true", help="")
+    parser.add_argument("-nl", "--Newfoundland", action="store_true", help="")
+    parser.add_argument("-yt", "--Yukon", action="store_true", help="")
+    parser.add_argument("-nt", "--NorthwestTerritories", action="store_true", help="")
+    parser.add_argument("-nu", "--Nunavut", action="store_true", help="")
+    parser.add_argument("-all", "--All", action="store_true", help="")
+    parser.add_argument("-prov", "--Province", help="")
+
+    args = parser.parse_args()
     rows = getData(sources.ab_url, sources.ab)
     data = parseData(rows)
-    print("Nominal print to stop debugger")
 
 
 if __name__ == "__main__":
